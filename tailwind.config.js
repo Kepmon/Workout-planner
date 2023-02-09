451006aea458435db23a9dda570feffacbb46479
Monika <monika.kepasa@gmail.com>
2023-02-09 01:46:37 +0100

    added a new plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue, js}'],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
   theme: {
     extend: {
       fontFamily: {
